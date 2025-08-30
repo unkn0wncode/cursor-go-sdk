43e47014f8a161b9b83931d48cd6510895218895
unkn0wncode <unkn0wncode@users.noreply.github.com>
2025-08-30 15:03:31 +0000

    Improve doc info

--- github.go
+++ github.go
@@ -5,7 +5,10 @@ import (
 )
 
 // ListRepositories returns GitHub repositories available to the authenticated user.
-// Matches OpenAPI /v0/repositories without query parameters.
+// This endpoint has very strict rate limits.
+// Limit requests to 1 / user / minute, and 30 / user / hour.
+// This request can take tens of seconds to respond for users with access to many repositories.
+// Make sure to handle this information not being available gracefully.
 func (c *Client) ListRepositories(ctx context.Context) (*ListRepositoriesResponse, error) {
 	var out ListRepositoriesResponse
 	if err := c.do(ctx, "GET", "/v0/repositories", nil, nil, &out); err != nil {
